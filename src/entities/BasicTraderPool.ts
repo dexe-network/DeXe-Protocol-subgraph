f4c0f7b3d297008825100852de6c8ab6cfc0be8c
Volodymyr <zolotukhinvolodymyr@gmail.com>
2021-12-03 12:01:36 +0200

    null -> Address.zero()

--- src/entities/BasicTraderPool.ts
+++ src/entities/BasicTraderPool.ts
@@ -1,18 +1,16 @@
-import { Address } from "@graphprotocol/graph-ts";
+import { Address} from "@graphprotocol/graph-ts";
 import { BasicPool } from "../../generated/schema"; 
 
 export function getBasicTraderPool(
   poolAddress: Address,
-  basicTokenAddress: Address | null = null
+  basicTokenAddress: Address = Address.zero()
 ): BasicPool {
   let basicPool = BasicPool.load(poolAddress.toHex());
 
   if (basicPool == null) {
     basicPool = new BasicPool(poolAddress.toHex());
 
-    if (basicTokenAddress != null) { 
-      basicPool.baseToken = basicTokenAddress;
-    }
+    basicPool.baseToken = basicTokenAddress;
   }
 
   return basicPool;
