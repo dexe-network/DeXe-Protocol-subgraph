8b981a927f1a325aab6064e81ce0cec910daae1a
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-02-08 12:45:41 +0200

    added safe calls

--- BasicPool/src/mappings/TraderPoolRegistry.ts
+++ BasicPool/src/mappings/TraderPoolRegistry.ts
@@ -7,36 +7,41 @@ import { BASIC_POOL_NAME, CHECK_PER_BLOCK, POOL_OFFSET, POOL_REGISTRY_ADDRESS } 
 export function handl(block: ethereum.Block): void {
   if (block.number.mod(BigInt.fromU64(CHECK_PER_BLOCK)).equals(BigInt.fromI32(0))) {
     let tprPrototype = TraderPoolRegistry.bind(Address.fromString(POOL_REGISTRY_ADDRESS));
-    let poolCount = tprPrototype.countPools(BASIC_POOL_NAME);
-    let iters = Math.ceil(F64.parseFloat(poolCount.toI64().toString()) / POOL_OFFSET);
+    let poolCount = tprPrototype.try_countPools(BASIC_POOL_NAME);
+
+    if (poolCount.reverted) return;
+
+    let iters = Math.ceil(F64.parseFloat(poolCount.value.toI64().toString()) / POOL_OFFSET);
 
     for (let i = 0; i < iters; i++) {
-      let poolInfo = tprPrototype.listPoolsWithInfo(
+      let poolInfo = tprPrototype.try_listPoolsWithInfo(
         BASIC_POOL_NAME,
         BigInt.fromI32(POOL_OFFSET * i),
         BigInt.fromI32((i + 1) * POOL_OFFSET)
       );
 
-      for (let pool = 0; pool < poolInfo.value0.length; pool++) {
-        let basicPool = getBasicTraderPool(poolInfo.value0[pool]);
-        let history = getBasicPoolPriceHistory(
-          basicPool.id,
-          poolInfo.value1[pool].totalPoolUSD,
-          block.number,
-          block.timestamp,
-          poolInfo.value1[pool].lpEmission,
-          poolInfo.value1[pool].totalPoolBase
-        );
+      if (!poolInfo.reverted) {
+        for (let pool = 0; pool < poolInfo.value.value0.length; pool++) {
+          let basicPool = getBasicTraderPool(poolInfo.value.value0[pool]);
+          let history = getBasicPoolPriceHistory(
+            basicPool.id,
+            poolInfo.value.value1[pool].totalPoolUSD,
+            block.number,
+            block.timestamp,
+            poolInfo.value.value1[pool].lpEmission,
+            poolInfo.value.value1[pool].totalPoolBase
+          );
 
-        let prevHistory = getBasicPoolPriceHistory(
-          basicPool.id,
-          poolInfo.value1[pool].totalPoolUSD,
-          block.number.minus(BigInt.fromI32(CHECK_PER_BLOCK))
-        );
+          let prevHistory = getBasicPoolPriceHistory(
+            basicPool.id,
+            poolInfo.value.value1[pool].totalPoolUSD,
+            block.number.minus(BigInt.fromI32(CHECK_PER_BLOCK))
+          );
 
-        history.loss = prevHistory.price.minus(history.price);
+          history.loss = prevHistory.price.minus(history.price);
 
-        history.save();
+          history.save();
+        }
       }
     }
   }
--- InvestPool/src/mappings/TraderPoolRegistry.ts
+++ InvestPool/src/mappings/TraderPoolRegistry.ts
@@ -7,36 +7,40 @@ import { CHECK_PER_BLOCK, INVEST_POOL_NAME, POOL_OFFSET, POOL_REGISTRY_ADDRESS }
 export function handl(block: ethereum.Block): void {
   if (block.number.mod(BigInt.fromU64(CHECK_PER_BLOCK)).equals(BigInt.fromI32(0))) {
     let tprPrototype = TraderPoolRegistry.bind(Address.fromString(POOL_REGISTRY_ADDRESS));
-    let poolCount = tprPrototype.countPools(INVEST_POOL_NAME);
-    let iters = Math.ceil(F64.parseFloat(poolCount.toI64().toString()) / POOL_OFFSET);
+    let poolCount = tprPrototype.try_countPools(INVEST_POOL_NAME);
+
+    if (poolCount.reverted) return;
+
+    let iters = Math.ceil(F64.parseFloat(poolCount.value.toI64().toString()) / POOL_OFFSET);
 
     for (let i = 0; i < iters; i++) {
-      let poolInfo = tprPrototype.listPoolsWithInfo(
+      let poolInfo = tprPrototype.try_listPoolsWithInfo(
         INVEST_POOL_NAME,
         BigInt.fromI32(POOL_OFFSET * i),
         BigInt.fromI32((i + 1) * POOL_OFFSET)
       );
+      if (!poolInfo.reverted) {
+        for (let pool = 0; pool < poolInfo.value.value0.length; pool++) {
+          let investPool = getInvestTraderPool(poolInfo.value.value0[pool]);
+          let history = getInvestPoolPriceHistory(
+            investPool.id,
+            poolInfo.value.value1[pool].totalPoolUSD,
+            block.number,
+            block.timestamp,
+            poolInfo.value.value1[pool].lpEmission,
+            poolInfo.value.value1[pool].totalPoolBase
+          );
 
-      for (let pool = 0; pool < poolInfo.value0.length; pool++) {
-        let investPool = getInvestTraderPool(poolInfo.value0[pool]);
-        let history = getInvestPoolPriceHistory(
-          investPool.id,
-          poolInfo.value1[pool].totalPoolUSD,
-          block.number,
-          block.timestamp,
-          poolInfo.value1[pool].lpEmission,
-          poolInfo.value1[pool].totalPoolBase
-        );
+          let prevHistory = getInvestPoolPriceHistory(
+            investPool.id,
+            poolInfo.value.value1[pool].totalPoolUSD,
+            block.number.minus(BigInt.fromI32(CHECK_PER_BLOCK))
+          );
 
-        let prevHistory = getInvestPoolPriceHistory(
-          investPool.id,
-          poolInfo.value1[pool].totalPoolUSD,
-          block.number.minus(BigInt.fromI32(CHECK_PER_BLOCK))
-        );
+          history.loss = prevHistory.price.minus(history.price);
 
-        history.loss = prevHistory.price.minus(history.price);
-
-        history.save();
+          history.save();
+        }
       }
     }
   }
