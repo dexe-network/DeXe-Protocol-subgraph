b108dc059c7347b3602d706e53ff842eb6155c7c
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-06-02 18:37:27 +0300

    added aggregation feature

--- AllPools/src/mappings/TraderPoolRegistry.ts
+++ AllPools/src/mappings/TraderPoolRegistry.ts
@@ -8,9 +8,37 @@ import {
   POOL_OFFSET,
   POOL_REGISTRY_ADDRESS,
   INVEST_POOL_NAME,
+  BLOCK_PER_5MIN,
 } from "../entities/global/globals";
 
+const BLOCK_INTERVALS = [
+  BLOCK_PER_5MIN.times(BigInt.fromI32(3)), // 15 min
+  BLOCK_PER_5MIN.times(BigInt.fromI32(6)), // 30 min
+  BLOCK_PER_5MIN.times(BigInt.fromI32(9)), // 45 min
+  BLOCK_PER_5MIN.times(BigInt.fromI32(12)), // 1 h
+  BLOCK_PER_5MIN.times(BigInt.fromI32(24)), // 2 h
+  BLOCK_PER_5MIN.times(BigInt.fromI32(48)), // 4 h
+  BLOCK_PER_5MIN.times(BigInt.fromI32(72)), // 6 h
+  BLOCK_PER_5MIN.times(BigInt.fromI32(144)), // 12 h
+  BLOCK_PER_5MIN.times(BigInt.fromI32(288)), // 24 h
+  BLOCK_PER_5MIN.times(BigInt.fromI32(288)).times(BigInt.fromI32(30)), // 1 m
+];
+
+const CODES = [
+  0b0000000001, // = 1   => 15 min
+  0b0000000010, // = 2   => 30 min
+  0b0000000100, // = 4   => 45 min
+  0b0000001000, // = 8   => 1 h
+  0b0000010000, // = 16  => 2 h
+  0b0000100000, // = 32  => 4 h
+  0b0001000000, // = 64  => 6 h
+  0b0010000000, // = 128 => 12 h
+  0b0100000000, // = 256 => 24 h
+  0b1000000000, // = 512 => 1 m
+];
+
 function updatePools(block: ethereum.Block, type: string): void {
+  let aggregationType = 0b0000;
   let tprPrototype = TraderPoolRegistry.bind(Address.fromString(POOL_REGISTRY_ADDRESS));
   let poolCount = tprPrototype.try_countPools(type);
 
@@ -28,6 +56,13 @@ function updatePools(block: ethereum.Block, type: string): void {
     if (!poolInfo.reverted) {
       for (let pool = 0; pool < poolInfo.value.value0.length; pool++) {
         let traderPool = getTraderPool(poolInfo.value.value0[pool]);
+
+        for (let i = 0; i < BLOCK_INTERVALS.length; i++) {
+          if (block.number.minus(traderPool.creationBlock).mod(BLOCK_INTERVALS[i]).equals(BigInt.zero())) {
+            aggregationType += CODES[i];
+          }
+        }
+
         let history = getTraderPoolPriceHistory(
           traderPool,
           block.number,
@@ -36,7 +71,8 @@ function updatePools(block: ethereum.Block, type: string): void {
           poolInfo.value.value1[pool].totalPoolBase,
           poolInfo.value.value1[pool].lpSupply,
           poolInfo.value.value1[pool].traderUSD,
-          poolInfo.value.value1[pool].traderBase
+          poolInfo.value.value1[pool].traderBase,
+          BigInt.fromI32(aggregationType)
         );
 
         history.save();
