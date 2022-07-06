bf185d5d6a0b9ad187fe30392d917309a4d9779d
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-07-06 17:30:13 +0300

    InvestPool updated to version 0.32.0

--- InvestPool/src/entities/invest-pool/proposal/Proposal.ts
+++ InvestPool/src/entities/invest-pool/proposal/Proposal.ts
@@ -9,7 +9,7 @@ export function getProposal(
   timestampLimit: BigInt = BigInt.zero(),
   investLPLimit: BigInt = BigInt.zero()
 ): Proposal {
-  let id = proposalContract.id + index.toString();
+  let id = proposalContract.id.toHexString() + index.toString();
   let proposal = Proposal.load(id);
 
   if (proposal == null) {
