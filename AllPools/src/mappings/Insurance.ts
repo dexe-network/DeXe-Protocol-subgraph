a21d7862220bde68eddb326b1b4f26175f9b5d94
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-07-13 12:14:25 +0300

    rm deposit, withdraw

--- AllPools/src/mappings/Insurance.ts
+++ AllPools/src/mappings/Insurance.ts
@@ -1,17 +1,5 @@
-import { Deposited, Paidout, Withdrawn } from "../../generated/Insurance/Insurance";
+import { Paidout } from "../../generated/Insurance/Insurance";
 import { getInvestor } from "../entities/trader-pool/Investor";
-
-export function onDeposit(event: Deposited): void {
-  let investor = getInvestor(event.params.investor);
-  investor.insurance = investor.insurance.plus(event.params.amount);
-  investor.save();
-}
-
-export function onWithdraw(event: Withdrawn): void {
-  let investor = getInvestor(event.params.investor);
-  investor.insurance = investor.insurance.minus(event.params.amount);
-  investor.save();
-}
 
 export function onPayout(event: Paidout): void {
   let investor = getInvestor(event.params.investor);
