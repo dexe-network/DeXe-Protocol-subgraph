cc1cb4ceaa11e4a8ea3d607b145152d39fde2dfa
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-07-20 19:19:14 +0300

    tests before fix

---
+++ InvestPool/tests/TraderPoolInvestProposal.test.ts
@@ -0,0 +1,249 @@
+import {
+  afterEach,
+  assert,
+  beforeAll,
+  beforeEach,
+  clearStore,
+  createMockedFunction,
+  describe,
+  newMockEvent,
+  test,
+} from "matchstick-as/assembly/index";
+import { getBlock, getTransaction } from "./utils";
+import { Address, ethereum, BigInt, Bytes } from "@graphprotocol/graph-ts";
+import {
+  ProposalClaimed,
+  ProposalCreated,
+  ProposalCreatedProposalLimitsStruct,
+  ProposalSupplied,
+  ProposalWithdrawn,
+} from "../generated/templates/InvestProposal/InvestProposal";
+import { getInvestTraderPool } from "../src/entities/invest-pool/InvestTraderPool";
+import { getProposalContract } from "../src/entities/invest-pool/proposal/ProposalContract";
+import {
+  onProposalCreated,
+  onProposalClaimed,
+  onProposalSupplied,
+  onProposalWithdrawn,
+} from "../src/mappings/TraderPoolInvestProposal";
+import { PRICE_FEED_ADDRESS } from "../src/entities/global/globals";
+
+function createProposalCreated(
+  proposalId: BigInt,
+  proposalLimits: ProposalCreatedProposalLimitsStruct,
+  sender: Address,
+  block: ethereum.Block,
+  tx: ethereum.Transaction
+): ProposalCreated {
+  let event = changetype<ProposalCreated>(newMockEvent());
+  event.parameters = new Array();
+
+  event.parameters.push(new ethereum.EventParam("proposalId", ethereum.Value.fromUnsignedBigInt(proposalId)));
+  event.parameters.push(new ethereum.EventParam("proposalLimits", ethereum.Value.fromTuple(proposalLimits)));
+
+  event.block = block;
+  event.transaction = tx;
+  event.address = sender;
+
+  return event;
+}
+
+function createProposalWithdrawn(
+  proposalId: BigInt,
+  user: Address,
+  amount: BigInt,
+  sender: Address,
+  block: ethereum.Block,
+  tx: ethereum.Transaction
+): ProposalWithdrawn {
+  let event = changetype<ProposalWithdrawn>(newMockEvent());
+  event.parameters = new Array();
+
+  event.parameters.push(new ethereum.EventParam("proposalId", ethereum.Value.fromUnsignedBigInt(proposalId)));
+  event.parameters.push(new ethereum.EventParam("sender", ethereum.Value.fromAddress(user)));
+  event.parameters.push(new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount)));
+
+  event.block = block;
+  event.transaction = tx;
+  event.address = sender;
+
+  return event;
+}
+
+function createProposalSupplied(
+  proposalId: BigInt,
+  user: Address,
+  amounts: Array<BigInt>,
+  tokens: Array<Address>,
+  sender: Address,
+  block: ethereum.Block,
+  tx: ethereum.Transaction
+): ProposalSupplied {
+  let event = changetype<ProposalSupplied>(newMockEvent());
+  event.parameters = new Array();
+
+  event.parameters.push(new ethereum.EventParam("proposalId", ethereum.Value.fromUnsignedBigInt(proposalId)));
+  event.parameters.push(new ethereum.EventParam("sender", ethereum.Value.fromAddress(user)));
+  event.parameters.push(new ethereum.EventParam("amounts", ethereum.Value.fromUnsignedBigIntArray(amounts)));
+  event.parameters.push(new ethereum.EventParam("tokens", ethereum.Value.fromAddressArray(tokens)));
+
+  event.block = block;
+  event.transaction = tx;
+  event.address = sender;
+
+  return event;
+}
+
+function createProposalClaimed(
+  proposalId: BigInt,
+  user: Address,
+  amounts: Array<BigInt>,
+  tokens: Array<Address>,
+  sender: Address,
+  block: ethereum.Block,
+  tx: ethereum.Transaction
+): ProposalClaimed {
+  let event = changetype<ProposalClaimed>(newMockEvent());
+  event.parameters = new Array();
+
+  event.parameters.push(new ethereum.EventParam("proposalId", ethereum.Value.fromUnsignedBigInt(proposalId)));
+  event.parameters.push(new ethereum.EventParam("user", ethereum.Value.fromAddress(user)));
+  event.parameters.push(new ethereum.EventParam("amounts", ethereum.Value.fromUnsignedBigIntArray(amounts)));
+  event.parameters.push(new ethereum.EventParam("tokens", ethereum.Value.fromAddressArray(tokens)));
+
+  event.block = block;
+  event.transaction = tx;
+  event.address = sender;
+
+  return event;
+}
+
+const block = getBlock(BigInt.fromI32(1), BigInt.fromI32(1));
+const tx = getTransaction(Bytes.fromByteArray(Bytes.fromBigInt(BigInt.fromI32(1))));
+const sender = Address.fromString("0x86e08f7d84603AEb97cd1c89A80A9e914f181679");
+const proposalId = BigInt.fromI32(1);
+const pool = Address.fromString("0x86e08f7d84603AEb97cd1c89A80A9e914f181670");
+const expectedUSD = BigInt.fromI32(10).pow(18);
+
+describe("TraderPoolInvestProposal", () => {
+  beforeAll(() => {
+    createMockedFunction(
+      Address.fromString(PRICE_FEED_ADDRESS),
+      "getNormalizedPriceOutUSD",
+      "getNormalizedPriceOutUSD(address,uint256):(uint256,address[])"
+    )
+      .withArgs([
+        ethereum.Value.fromAddress(Address.fromString("0x86e08f7d84603aeb97cd1c89a80a9e914f181979")),
+        ethereum.Value.fromUnsignedBigInt(expectedUSD),
+      ])
+      .returns([ethereum.Value.fromUnsignedBigInt(expectedUSD), ethereum.Value.fromAddressArray([sender, sender])]);
+  });
+
+  beforeEach(() => {
+    let traderPool = getInvestTraderPool(pool);
+    traderPool.save();
+    let proposal = getProposalContract(sender, pool);
+    proposal.save();
+  });
+
+  afterEach(() => {
+    clearStore();
+  });
+
+  test("should handle ProposalCreated event", () => {
+    let proposalLimits = new ProposalCreatedProposalLimitsStruct(3);
+    proposalLimits[0] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(1));
+    proposalLimits[1] = ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(2));
+
+    let event = createProposalCreated(proposalId, proposalLimits, sender, block, tx);
+
+    onProposalCreated(event);
+
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "investPool", pool.toHexString());
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "timestampLimit", "1");
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "investLPLimit", "2");
+
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "leftTokens", "[]]");
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "leftAmounts", "[]");
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "totalUSDSupply", "0");
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "firstSupplyTimestamp", "0");
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "APR", "0");
+
+    assert.fieldEquals(
+      "Proposal",
+      sender.toHexString() + proposalId.toString(),
+      "lastSupply",
+      sender.toHexString() + proposalId.toString()
+    );
+    assert.fieldEquals(
+      "Proposal",
+      sender.toHexString() + proposalId.toString(),
+      "lastWithdraw",
+      sender.toHexString() + proposalId.toString()
+    );
+  });
+
+  test("should handle ProposalWithdrawn event", () => {
+    let user = Address.fromString("0x86e08f7d84603AEb97cd1c89A85A9e914f181679");
+    let amount = BigInt.fromI32(10).pow(18);
+
+    let event = createProposalWithdrawn(proposalId, user, amount, sender, block, tx);
+
+    onProposalWithdrawn(event);
+
+    assert.fieldEquals("LastWithdrawn", sender.toHexString() + proposalId.toString(), "amountBase", amount.toString());
+  });
+
+  test("should handle ProposalSupplied event", () => {
+    let user = Address.fromString("0x86e08f7d84603AEb97cd1c89A85A9e914f181679");
+    let tokens = [
+      Address.fromString("0x86e08f7d84603AEb97cd1c89A85A9e914f181670"),
+      Address.fromString("0x86e08f7d84603AEb97cd1c89A85A9e914f181671"),
+    ];
+    let amounts = [BigInt.fromI32(10).pow(18), BigInt.fromI32(10).pow(19)];
+
+    let event = createProposalSupplied(proposalId, user, amounts, tokens, sender, block, tx);
+
+    onProposalSupplied(event);
+
+    assert.fieldEquals(
+      "LastSupply",
+      sender.toHexString() + proposalId.toString(),
+      "dividendsTokens",
+      "[" + tokens[0].toHexString() + ", " + tokens[1].toHexString() + "]"
+    );
+    assert.fieldEquals(
+      "LastSupply",
+      sender.toHexString() + proposalId.toString(),
+      "amountDividendsTokens",
+      "[" + amounts[0].toHexString() + ", " + amounts[1].toHexString() + "]"
+    );
+    assert.fieldEquals(
+      "LastSupply",
+      sender.toHexString() + proposalId.toString(),
+      "timestamp",
+      block.timestamp.toString()
+    );
+
+    assert.fieldEquals(
+      "Proposal",
+      sender.toHexString() + proposalId.toString(),
+      "totalUSDSupply",
+      expectedUSD.toString()
+    );
+    assert.fieldEquals("Proposal", sender.toHexString() + proposalId.toString(), "APR", "0");
+
+    assert.fieldEquals(
+      "Proposal",
+      sender.toHexString() + proposalId.toString(),
+      "lastSupply",
+      sender.toHexString() + proposalId.toString()
+    );
+    assert.fieldEquals(
+      "Proposal",
+      sender.toHexString() + proposalId.toString(),
+      "lastWithdraw",
+      sender.toHexString() + proposalId.toString()
+    );
+  });
+});
