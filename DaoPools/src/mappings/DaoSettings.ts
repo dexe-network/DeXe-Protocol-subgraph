9ad43689056f97ee046ae766dc86ce61a56d49f4
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-10-19 16:29:45 +0300

    added entities

---
+++ DaoPools/src/mappings/DaoSettings.ts
@@ -0,0 +1,5 @@
+import { ExecutorChanged, SettingsChanged } from "../../generated/templates/DaoSettings/DaoSettings";
+
+export function onSettingsChanged(event: SettingsChanged): void {}
+
+export function onExecutorChanged(event: ExecutorChanged): void {}
