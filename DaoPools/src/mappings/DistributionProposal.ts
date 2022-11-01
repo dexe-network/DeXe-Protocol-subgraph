6e48edfe483db4fe0bc7b9015853441cc0d006ee
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-11-01 14:00:19 +0200

    added correct addresses

--- DaoPools/src/mappings/DistributionProposal.ts
+++ DaoPools/src/mappings/DistributionProposal.ts
@@ -1,14 +1,16 @@
+import { Address } from "@graphprotocol/graph-ts";
 import { DistributionProposalClaimed } from "../../generated/templates/DistributionProposal/DistributionProposal";
 import { getDaoPool } from "../entities/DaoPool";
+import { getDPContract } from "../entities/DPContract";
 import { getProposal } from "../entities/Proposal";
 import { getVoter } from "../entities/Voters/Voter";
 import { getVoterInPool } from "../entities/Voters/VoterInPool";
-import { getVoterInProposal } from "../entities/Voters/VoterInProposal";
 import { extendArray } from "../helpers/ArrayHelper";
 
 export function onDistributionProposalClaimed(event: DistributionProposalClaimed): void {
+  let dpToPool = getDPContract(event.address);
   let voter = getVoter(event.params.sender);
-  let pool = getDaoPool(event.address);
+  let pool = getDaoPool(Address.fromString(dpToPool.daoPool.toHexString()));
   let voterInPool = getVoterInPool(pool, voter);
   let proposal = getProposal(pool, event.params.proposalId);
 
