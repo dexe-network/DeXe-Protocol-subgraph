7f7d6badab4bb2d17295183c1115dd4efef0c01a
Volodymyr <zolotukhinvolodymyr@gmail.com>
2022-11-03 19:10:16 +0200

    added proposal events handlers

--- DaoValidators/src/entities/Proposal.ts
+++ DaoValidators/src/entities/Proposal.ts
@@ -1,14 +1,26 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { DaoPool, Proposal } from "../../generated/schema";
 
-export function getProposal(pool: DaoPool, proposalId: BigInt): Proposal {
-  let id = pool.id.concatI32(proposalId.toI32());
+export function getProposal(
+  pool: DaoPool,
+  proposalId: BigInt,
+  isInternal: boolean,
+  description: string = "",
+  quorum: BigInt = BigInt.zero(),
+  creator: Bytes = Bytes.empty()
+): Proposal {
+  let id = pool.id.toHexString() + proposalId.toString() + "_" + (isInternal ? "1" : "0");
   let proposal = Proposal.load(id);
 
   if (proposal == null) {
     proposal = new Proposal(id);
     proposal.proposalId = proposalId;
+    proposal.isInternal = isInternal;
+    proposal.description = description;
+    proposal.quorum = quorum;
     proposal.totalVote = BigInt.zero();
+    proposal.executor = Bytes.empty();
+    proposal.creator = creator;
   }
 
   return proposal;
