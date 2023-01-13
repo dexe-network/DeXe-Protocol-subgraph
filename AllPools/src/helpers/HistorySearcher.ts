65035f579bcb446c9a1f3f4b2ca0251182f6ec98
Volodymyr <zolotukhinvolodymyr@gmail.com>
2023-01-13 16:43:44 +0200

    added MAX_SEARCH_DEPTH

--- AllPools/src/helpers/HistorySearcher.ts
+++ AllPools/src/helpers/HistorySearcher.ts
@@ -1,4 +1,5 @@
 import { BigInt } from "@graphprotocol/graph-ts";
+import { CHECK_PER_BLOCK, MAX_SEARCH_DEPTH } from "../entities/global/globals";
 
 export function findPrevHistory<T>(
   loadFunction: (id: string) => T | null,
@@ -10,7 +11,11 @@ export function findPrevHistory<T>(
   let newSuffix = idSuffix.minus(decrement);
   let history = loadFunction(idBase + newSuffix.toString());
 
-  while (history == null && newSuffix.gt(minimum)) {
+  while (
+    history == null &&
+    newSuffix.gt(minimum) &&
+    idSuffix.minus(newSuffix).div(BigInt.fromI32(CHECK_PER_BLOCK)).le(BigInt.fromI32(MAX_SEARCH_DEPTH))
+  ) {
     newSuffix = newSuffix.minus(decrement);
     history = loadFunction(idBase + newSuffix.toString());
   }
