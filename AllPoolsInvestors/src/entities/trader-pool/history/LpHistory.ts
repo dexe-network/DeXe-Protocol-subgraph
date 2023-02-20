400459e4d7fd0976605462349ff17b56c3d799db
Volodymyr <zolotukhinvolodymyr@gmail.com>
2023-02-20 18:53:15 +0200

    added amounts history

--- AllPoolsInvestors/src/entities/trader-pool/history/LpHistory.ts
+++ AllPoolsInvestors/src/entities/trader-pool/history/LpHistory.ts
@@ -1,6 +1,7 @@
 import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { InvestorPoolPosition, LpHistory } from "../../../../generated/schema";
 import { DAY } from "../../global/globals";
+import { findPrevHistory } from "../../../helpers/HistorySearcher";
 
 export function getLpHistory(investorPoolPosition: InvestorPoolPosition, timestamp: BigInt): LpHistory {
   let day = timestamp.div(BigInt.fromI32(DAY));
@@ -17,4 +18,19 @@ export function getLpHistory(investorPoolPosition: InvestorPoolPosition, timesta
   }
 
   return history;
+}
+
+export function injectPrevLPHistory(history: LpHistory, investorPoolPosition: InvestorPoolPosition): void {
+  if (history.prevHistory == "") {
+    let prevHistory = findPrevHistory<LpHistory>(
+      LpHistory.load,
+      investorPoolPosition.id,
+      history.day,
+      BigInt.fromI32(1)
+    );
+    if (prevHistory != null) {
+      history.prevHistory = prevHistory.id;
+      history.currentLpAmount = prevHistory.currentLpAmount;
+    }
+  }
 }
