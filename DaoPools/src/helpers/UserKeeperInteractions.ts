b077aea3c9606a4c28e3046d71e6e8a45f14ef9b
Anton Chaika <achayka95@gmail.com>
2023-09-11 14:41:12 +0300

    small fix

--- DaoPools/src/helpers/UserKeeperInteractions.ts
+++ DaoPools/src/helpers/UserKeeperInteractions.ts
@@ -5,11 +5,13 @@ export function getNftsVotePower(userKeeper: Address, nftIds: BigInt[]): BigInt 
   let pfPrototype = UserKeeper.bind(userKeeper);
 
   let resp = pfPrototype.try_nftVotingPower(nftIds, false);
+
   if (resp.reverted) {
     log.warning("try_nftVotingPower reverted. UserKeeper: {}, nftIds: {}", [
       userKeeper.toHexString(),
       nftIds.toString(),
     ]);
+
     return BigInt.zero();
   } else {
     if (resp.value.value0.length == 0) {
